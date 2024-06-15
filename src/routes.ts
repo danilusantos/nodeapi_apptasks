aab4de4574198e6a065c54e947b421825f0c6853
Danilo Santos <danilondosantos@gmail.com>
2024-06-14 21:01:18 -0300

    build: :construction_worker: adjust gitignore and routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,25 @@
-import { Router, request, response, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 
-import { getTasks } from './controller/TaskController';
-import { saveTask } from './controller/TaskController';
-import { getTask } from './controller/TaskController';
-import { updateTask } from './controller/TaskController';
-import { deleteTask } from './controller/TaskController';
-import { finishedTask } from './controller/TaskController';
+import {
+  getTasks,
+  saveTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  finishedTask
+} from "./controller/TaskController";
 
 const routes = Router();
 
-routes.get('/home', (request: Request, response: Response) => {
-  return response.redirect('/tasks');
+routes.get("/", (req: Request, res: Response) => {
+  return res.json({ message: 'Olá mundo!' });
 });
 
-routes.get('/tasks', getTasks);
-routes.post('/tasks', saveTask);
-routes.get('/tasks/:id', getTask);
-routes.put('/tasks/:id', updateTask);
-routes.delete('/tasks/:id', deleteTask);
-routes.patch('/tasks/:id', finishedTask);
+routes.get("/tasks", getTasks);
+routes.post("/tasks", saveTask);
+routes.get("/tasks/:id", getTask);
+routes.put("/tasks/:id", updateTask);
+routes.delete("/tasks/:id", deleteTask);
+routes.patch("/tasks/:id", finishedTask);
 
 export default routes;
