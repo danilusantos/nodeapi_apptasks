1bf687ca9b43d4493d8f5277ef38e1399be73ab9
Danilo Santos <danilondosantos@gmail.com>
2024-06-14 22:24:04 -0300

    fix: :bug: fix logical for production

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,14 +5,14 @@ import { Task } from './entity/Task';
 const PostgresAppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.ENV === 'PROD'
-    ? 'dpg-cpi7fl21hbls73bdh4v0-a.oregon-postgres.render.com'
-    : 'dpg-cpi7fl21hbls73bdh4v0-a',
+    ? 'dpg-cpi7fl21hbls73bdh4v0-a'
+    : 'dpg-cpi7fl21hbls73bdh4v0-a.oregon-postgres.render.com',
   port: 5432,
   username: 'db_tarefas_oo8z_user',
   password: 'AeUcgYjbX2Jhh6F8gXDus7oPdLB1mIFL',
   database: 'db_tarefas_oo8z',
   synchronize: true,
-  logging: true,
+  logging: false,
   entities: [Task],
   migrations: [],
   subscribers: [],
