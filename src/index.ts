fd8ffa0669716ccd75a790e6aa52691dd6fe436a
Danilo Santos <danilondosantos@gmail.com>
2024-06-14 23:40:44 -0300

    feat: :sparkles: adding cors for all routes

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,25 @@ import routes from './routes';
 AppDataSource.initialize();
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+
+// Rotas
 app.use(routes);
 
-app.listen(port);
+// Middleware para tratar rotas não encontradas (404)
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Rota não encontrada' });
+});
+
+// Middleware para tratamento global de erros
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Ocorreu um erro inferno no servidor' });
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
