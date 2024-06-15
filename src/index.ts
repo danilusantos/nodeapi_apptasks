9378a402d727aec6de18c2d5ff11b80a6f38b5d6
Danilo Santos <danilondosantos@gmail.com>
2024-06-14 21:12:15 -0300

    fix: :bug: defining port in prod

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,9 @@ import routes from './routes';
 AppDataSource.initialize();
 
 const app = express();
+const port = process.env.PORT || 4000
 
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port);
